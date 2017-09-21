4eb9db1549fe852023bf935542ea201ea24a4dba
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2017-09-21 15:28:03 +0800

    auth

--- js/Authentication.js
+++ js/Authentication.js
@@ -17,12 +17,30 @@ function onClick(){
     console.log("email: "+email);
     console.log("password: "+password);
     //註冊新用戶
+    var check=0;
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        //錯誤檢查
+        check=1;
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log(errorCode+" "+errorMessage)
+        console.log(errorCode+" | "+errorMessage+" | "+error)
+        
         // ...
     });
-    alert("Finish!");
+    setTimeout(function(){
+        if(check==1)
+            alert("錯誤！請確認格式");
+        else{
+            document.location.href="../seeAuth.html";
+            //寫入database
+            var msgRef =firebase.database().ref('/Auth/');
+            msgRef.push({
+            messages:email
+            })
+        }
+        
+    },1000);
+ 
+    
 }
