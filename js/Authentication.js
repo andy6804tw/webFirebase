c50d289794de7f5f7db18f0fbb446c41126caa74
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2017-09-21 15:57:22 +0800

    test

--- js/Authentication.js
+++ js/Authentication.js
@@ -12,35 +12,36 @@ firebase.initializeApp(config);
 
 
 function onClick(){
+    alert("sfs");
     var email=document.getElementById("email").value;
     var password=document.getElementById("password").value;
     console.log("email: "+email);
     console.log("password: "+password);
-    //註冊新用戶
-    var check=0;
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        //錯誤檢查
-        check=1;
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode+" | "+errorMessage+" | "+error)
+    // //註冊新用戶
+    // var check=0;
+    // firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    //     //錯誤檢查
+    //     check=1;
+    //     // Handle Errors here.
+    //     var errorCode = error.code;
+    //     var errorMessage = error.message;
+    //     console.log(errorCode+" | "+errorMessage+" | "+error)
         
-        // ...
-    });
-    setTimeout(function(){
-        if(check==1)
-            alert("錯誤！請確認格式");
-        else{
-            document.location.href="../seeAuth.html";
-            //寫入database
-            var msgRef =firebase.database().ref('/Auth/');
-            msgRef.push({
-            messages:email
-            })
-        }
+    //     // ...
+    // });
+    // setTimeout(function(){
+    //     if(check==1)
+    //         alert("錯誤！請確認格式");
+    //     else{
+    //         document.location.href="../seeAuth.html";
+    //         //寫入database
+    //         var msgRef =firebase.database().ref('/Auth/');
+    //         msgRef.push({
+    //         messages:email
+    //         })
+    //     }
         
-    },1000);
+    // },1000);
  
     
 }
